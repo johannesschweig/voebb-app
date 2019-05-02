b1da59766328744b2b98f3875e9ebee61deb165c
Johannes Schweig <johannes.schweig@knime.com>
2019-05-02 18:25:25 +0200

    CTRL+SHIFT+I now opens dev tools in built app

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,9 +1,20 @@
 import Vue from 'vue'
 import App from './App'
 import store from './store/store.js'
+import { remote } from 'electron'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
+
+console.log('User storage in', require('electron-json-storage').getDefaultDataPath())
+
+// register shortcut to open dev tools
+remote.globalShortcut.register('CommandOrControl+Shift+I', () => {
+    remote.BrowserWindow.getFocusedWindow().webContents.openDevTools()
+})
+window.addEventListener('beforeunload', () => {
+    remote.globalShortcut.unregisterAll()
+})
 
 /* eslint-disable no-new */
 new Vue({
